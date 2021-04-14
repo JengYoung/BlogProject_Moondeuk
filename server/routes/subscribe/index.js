bc2254f0b442b54e64cad88fad25b8c0f50a2ebb
Jengyoung <young_pallete@naver.com>
2021-04-14 13:42:36 +0900

    unSubscribeController 구현

--- server/routes/subscribe/index.js
+++ server/routes/subscribe/index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import subscribeController from './subscribeController.js';
 import subscribedInfoController from './subscribedInfoController.js';
 import subscribeInfoController from './subscribeInfoController.js';
+import unSubscribeController from './unSubscribeController.js';
 
 const subscribe = express.Router();
 
 subscribe.post('/subscribeInfo', subscribeInfoController);
 subscribe.post('/subscribe', subscribeController);
 subscribe.post('/subscribedInfo', subscribedInfoController);
-// subscribe.post('/subscribedList', subscribedListController);
-// subscribe.post('/unsubscribe', unsubscribeController);
+subscribe.post('/unsubscribe', unSubscribeController);
 
 export default subscribe;
