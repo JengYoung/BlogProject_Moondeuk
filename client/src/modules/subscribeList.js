392c7858fba208b3af555fcfbcb2b9c668c4bfaa
Jengyoung <young_pallete@naver.com>
2021-04-20 00:01:37 +0900

    added - module / subscribedList action & reducer

--- client/src/modules/subscribeList.js
+++ client/src/modules/subscribeList.js
@@ -3,6 +3,7 @@ import createActionTypes from '../lib/createActionTypes';
 import createSaga from '../lib/createSaga';
 import subscribeListAPI from '../lib/routes/subscribe/subscribeList';
 import { takeLatest } from 'redux-saga/effects';
+import subscribedListAPI from '../lib/routes/subscribe/subscribedList';
 
 /* 
     create Action Types
@@ -14,14 +15,20 @@ import { takeLatest } from 'redux-saga/effects';
 const INITIALIZE_SUBSCRIBE = 'subscribeList/INITIALIZE_SUBSCRIBE';
 const SUBSCRIBE_LIST = 'subscribeList/SUBSCRIBE_LIST';
 const [ SUBSCRIBE_LIST_SUCCESS, SUBSCRIBE_LIST_FAILURE ] = createActionTypes(SUBSCRIBE_LIST);
+const SUBSCRIBED_LIST = 'subscribeList/SUBSCRIBED_LIST';
+const [ SUBSCRIBED_LIST_SUCCESS, SUBSCRIBED_LIST_FAILURE ] = createActionTypes(SUBSCRIBED_LIST);
+
 
 export const initializeSubscribe = createAction(INITIALIZE_SUBSCRIBE);
 export const getSubscribeList = createAction(SUBSCRIBE_LIST, subscribeList => subscribeList);
+export const getSubsribedList = createAction(SUBSCRIBED_LIST, subscribedList => subscribedList);
 
 const getSubscribeListSaga = createSaga(SUBSCRIBE_LIST, subscribeListAPI);
+const getSubscribedListSaga = createSaga(SUBSCRIBED_LIST, subscribedListAPI);
 
 export function* subscribeListSaga() {
     yield takeLatest(SUBSCRIBE_LIST, getSubscribeListSaga);
+    yield takeLatest(SUBSCRIBED_LIST, getSubscribedListSaga);
 }
 
 const initialState = { 
@@ -43,6 +50,14 @@ const subscribeListReducer = handleActions({
         subscribeList
     }),
     [SUBSCRIBE_LIST_FAILURE]: (state, { payload: error }) => ({
+        ...state,
+        subscribeError: error,
+    }),
+    [SUBSCRIBED_LIST_SUCCESS]: (state, { payload: subscribedList }) => ({
+        ...state,
+        subscribedList
+    }),
+    [SUBSCRIBED_LIST_FAILURE]: (state, { payload: error }) => ({
         ...state,
         subscribeError: error,
     }),
