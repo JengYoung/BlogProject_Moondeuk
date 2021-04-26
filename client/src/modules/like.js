3dce98fbf604465124e90117de3cd2223d062ec4
Jengyoung <young_pallete@naver.com>
2021-04-26 17:08:46 +0900

    [ CREATE ] like Module

---
+++ client/src/modules/like.js
@@ -0,0 +1,39 @@
+import { createAction, handleActions } from 'redux-actions';
+import createActionTypes from '../lib/createActionTypes';
+import createSaga from '../lib/createSaga';
+import likeAPI from '../lib/routes/like/like';
+import { takeLatest } from 'redux-saga/effects';
+
+const INITIALIZE_LIKE = 'like/INITIALIZE_LIKE';
+const LIKE = 'like/LIKE';
+const [ LIKE_SUCCESS, LIKE_FAILURE ] = createActionTypes(LIKE);
+
+export const initializeLike = createAction(INITIALIZE_LIKE);
+export const likeUser = createAction(LIKE, like => like);
+
+const likeUserSaga = createSaga(LIKE, likeAPI);
+export function* likeSaga() {
+    yield takeLatest(LIKE, likeUserSaga);
+};
+
+const initialState = {
+    like: {
+        userId: null,
+        diaryId: null,
+    },
+    likeError: null,
+}
+const likeReducer = handleActions({
+    [INITIALIZE_LIKE]: state => initialState,
+    [LIKE_SUCCESS]: (state, { payload: like }) => ({
+        ...state,
+        like,
+        likeError: null,
+    }),
+    [LIKE_FAILURE]: (state, { payload: error }) => ({
+        ...state,
+        likeError: error,
+    }),
+}, initialState);
+
+export default likeReducer;
