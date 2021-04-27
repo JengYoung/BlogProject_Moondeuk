9a350d435a2a7293eb2e4c292368f09b9ef6fcc0
Jengyoung <young_pallete@naver.com>
2021-04-27 16:23:19 +0900

    [ UPDATE ] subscribeInfoController & subscribedInfoController error fixed

--- server/routes/subscribe/subscribeInfoController.js
+++ server/routes/subscribe/subscribeInfoController.js
@@ -4,12 +4,12 @@ import User from '../../models/user.js';
 /* 해당 유저가 추가한 이웃 리스트 및 수를 전달. */
 const subscribeInfoController = async (req, res) => {
     const { authorId } = req.params;
-    const {_id} = await User.checkUserId(authorId);
-    console.log( _id );
-    const subscribeList = await Subscribe.find({subscribedFrom: _id}).exec();
+    if (!authorId) return;
+    console.log("subscribeInfoController - authorId: ", authorId);
+    const checkId = await User.checkUserId(authorId)._id;
+    const subscribeList = await Subscribe.find({subscribedFrom: checkId}).exec();
     try {
         const result = await User.getUserIdAndNickname(subscribeList, 'subscribeTo');
-        console.log(result);
         return res.send({subscribeToList: result, count: subscribeList.length});
     } catch(e) {
         return res.status(400).send(e);
--- server/routes/subscribe/subscribedInfoController.js
+++ server/routes/subscribe/subscribedInfoController.js
@@ -4,13 +4,11 @@ import User from '../../models/user.js';
 /* 해당 유저가 추가한 이웃 리스트 및 수를 전달. */
 const subscribedInfoController = async (req, res) => {
     const { authorId } = req.params;
-    const {_id} = await User.checkUserId(authorId);
-    console.log(_id);
-    const subscribeList = await Subscribe.find({subscribeTo: _id}).exec();
+    const checkId = await User.checkUserId(authorId)._id;
+    const subscribeList = await Subscribe.find({subscribeTo: checkId}).exec();
     console.log(subscribeList);
     try {
         const result = await User.getUserIdAndNickname(subscribeList, 'subscribedFrom');
-        console.log(result);
         return res.send({subscribedFromList: result, count: subscribeList.length});
     } catch(e) {
         return res.status(400).send(e);
