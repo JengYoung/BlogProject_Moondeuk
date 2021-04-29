18087021c7baf2aca590cc5f652ec15212e5a412
Jengyoung <young_pallete@naver.com>
2021-04-29 10:06:11 +0900

    [ CREATE ] comment module

---
+++ client/src/modules/comment.js
@@ -0,0 +1,46 @@
+import { createAction, handleActions } from 'redux-actions';
+import createActionTypes from '../lib/createActionTypes';
+import createSaga from '../lib/createSaga';
+import commentAPI from '../lib/routes/comment/comment';
+import { takeLatest } from 'redux-saga/effects';
+
+/* Action to Comment on diary */ 
+const COMMENT = 'comment/COMMENT';
+const [ COMMENT_SUCCESS, COMMENT_FAILURE ] = createActionTypes(COMMENT);
+
+/* Action Creator */ 
+export const commentDiary = createAction(COMMENT, comment => comment);
+
+/* customized Saga */
+const commentDiarySaga = createSaga(COMMENT, commentAPI);
+
+/* Saga */ 
+export function* commentSaga() {
+    yield takeLatest(COMMENT, commentDiarySaga);
+};
+
+const initialState = {
+    user_id: null,
+    diary_id: null,
+    userInfo: {
+        userId: null,
+        nickname: null,
+    },
+    comment: '',
+    commentAt: null,
+    commentError: null,
+};
+
+const commentReducer = handleActions({
+    [COMMENT_SUCCESS]: (state, { payload: comment }) => ({
+        ...state,
+        comment,
+        commentError: null,
+    }),
+    [COMMENT_FAILURE]: (state, { payload: error }) => ({
+        ...state,
+        commentError: error,
+    }),
+}, initialState);
+
+export default commentReducer;
