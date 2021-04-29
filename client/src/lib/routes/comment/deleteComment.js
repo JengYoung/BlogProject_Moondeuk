3cf4f357365b1449933380ad1b6921bb1b72d77a
Jengyoung <young_pallete@naver.com>
2021-04-29 13:55:50 +0900

    [ UPDATE ] comment routes parameter error handling

--- client/src/lib/routes/comment/deleteComment.js
+++ client/src/lib/routes/comment/deleteComment.js
@@ -1,6 +1,6 @@
 import client from '../client';
 
 const deleteCommentAPI = commentId =>
-    client.delete(`/routes/comment/:${commentId}`);
+    client.delete(`/routes/comment/${commentId}`);
 
 export default deleteCommentAPI;
--- client/src/lib/routes/comment/updateComment.js
+++ client/src/lib/routes/comment/updateComment.js
@@ -1,6 +1,6 @@
 import client from '../client';
 
 const updateCommentAPI = commentId => 
-    client.patch(`/routes/comment/:${commentId}`);
+    client.patch(`/routes/comment/${commentId}`);
 
 export default updateCommentAPI;
