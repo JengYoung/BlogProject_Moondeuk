465d9159e59fd54497f7b0086d46dd343dc9646b
Jengyoung <young_pallete@naver.com>
2021-04-29 16:09:44 +0900

    [ CREATE ] checkComment reducer

--- client/src/modules/comment.js
+++ client/src/modules/comment.js
@@ -3,12 +3,15 @@ import createActionTypes from '../lib/createActionTypes';
 import createSaga from '../lib/createSaga';
 import commentAPI from '../lib/routes/comment/comment';
 import { takeLatest } from 'redux-saga/effects';
+import checkCommentAPI from '../lib/routes/comment/checkComment';
 
 /* Action to Comment on diary */ 
 const INITIALIZE_COMMENT = 'comment/INITIALIZE_COMMENT'
 const CHANGE_TEXT = 'comment/CHANGE_TEXT';
 const COMMENT = 'comment/COMMENT';
 const [ COMMENT_SUCCESS, COMMENT_FAILURE ] = createActionTypes(COMMENT);
+const CHECK_COMMENT = 'comment/COMMENT_LIST';
+const [ CHECK_COMMENT_SUCCESS, CHECK_COMMENT_FAILURE ] = createActionTypes(CHECK_COMMENT);
 
 /* Action Creator */ 
 export const initializeComment = createAction(INITIALIZE_COMMENT);
@@ -19,19 +22,24 @@ export const commentDiary = createAction(COMMENT, ({ user_id, diary_id, content 
     content,
 }));
 
+export const checkComment = createAction(CHECK_COMMENT, diaryId => diaryId);
+
 /* customized Saga */
 const commentDiarySaga = createSaga(COMMENT, commentAPI);
+const checkCommentSaga = createSaga(CHECK_COMMENT, checkCommentAPI);
 
 /* Saga */ 
 export function* commentSaga() {
     yield takeLatest(COMMENT, commentDiarySaga);
+    yield takeLatest(CHECK_COMMENT, checkCommentSaga);
 };
 
 const initialState = {
     content: '',
     comment: null,
-    comments: [],
     commentError: null,
+    comments: [],
+    commentsError: null,
 };
 
 const commentReducer = handleActions({
@@ -49,6 +57,15 @@ const commentReducer = handleActions({
         ...state,
         commentError: error,
     }),
+    [CHECK_COMMENT_SUCCESS]: (state, { payload: comments }) => ({
+        ...state,
+        comments,
+        commentsError: null
+    }),
+    [CHECK_COMMENT_FAILURE]: (state, { payload: error }) => ({
+        ...state,
+        commentsError: error
+    })
 }, initialState);
 
 export default commentReducer;
