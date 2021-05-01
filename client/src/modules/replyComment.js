d196afe44767a171946f4e97d2e8d09d3c8ff8a7
Jengyoung <young_pallete@naver.com>
2021-05-01 14:38:14 +0900

    [ CREATE ] checkReplyComment action & reducer

--- client/src/modules/replyComment.js
+++ client/src/modules/replyComment.js
@@ -3,6 +3,7 @@ import createActionTypes from '../lib/createActionTypes';
 import createSaga from '../lib/createSaga';
 import replyCommentAPI from '../lib/routes/replyComment/replyComment';
 import { takeLatest } from 'redux-saga/effects'
+import checkReplyCommentAPI from '../lib/routes/replyComment/checkReplyComment';
 
 
 /* create Action Types */ 
@@ -10,26 +11,30 @@ const CHANGE_REPLYCOMMENT_TEXT = `replyComment/CHANGE_REPLYCOMMENT_TEXT`
 const INITIALIZE_REPLYCOMMENT = 'replyComment/INITIALIZE_REPLYCOMMENT'
 const REPLYCOMMENT = 'replyComment/REPLYCOMMENT';
 const [ REPLYCOMMENT_SUCCESS, REPLYCOMMENT_FAILURE ] = createActionTypes(REPLYCOMMENT);
+const CHECK_REPLYCOMMENT = 'replyComment/CHECK_REPLYCOMMENT';
+const [ CHECK_REPLYCOMMENT_SUCCESS, CHECK_REPLYCOMMENT_FAILURE ] = createActionTypes(CHECK_REPLYCOMMENT);
 
 /* create Action Creator*/ 
-export const replyComment = createAction(REPLYCOMMENT, ({ user_id, comment_id, content }) => ({
-    user_id, 
-    comment_id, 
-    content
-}));
-
 export const initializeReplyComment = createAction(INITIALIZE_REPLYCOMMENT)
 export const changeReplyCommentText = createAction(CHANGE_REPLYCOMMENT_TEXT, ({ name, value }) => ({
     name, // content
     value, // updatedContent
 }));
+export const replyComment = createAction(REPLYCOMMENT, ({ user_id, comment_id, content }) => ({
+    user_id, 
+    comment_id, 
+    content
+}));
+export const checkReplyComment = createAction(CHECK_REPLYCOMMENT, commentId => commentId);
 
 /* create customized Saga */ 
 const replyCommentDiarySaga = createSaga(REPLYCOMMENT, replyCommentAPI);
+const checkReplyCommentDiarySaga = createSaga(CHECK_REPLYCOMMENT, checkReplyCommentAPI);
 
 /* create replyCommentSaga */ 
 export function* replyCommentSaga() {
     yield takeLatest(REPLYCOMMENT, replyCommentDiarySaga);
+    yield takeLatest(CHECK_REPLYCOMMENT, checkReplyCommentDiarySaga);
 };
 
 const initialState = {
@@ -56,6 +61,15 @@ const replyCommentReducer = handleActions({
         ...state,
         replyCommentError: error,
     }),
+    [CHECK_REPLYCOMMENT_SUCCESS]: (state, { payload: replyComments }) => ({
+        ...state,
+        replyComments,
+        replyCommentsError: null,
+    }),
+    [CHECK_REPLYCOMMENT_FAILURE]: (state, { payload: error }) => ({
+        ...state,
+        replyCommentsError: error,
+    }) 
 }, initialState);
 
 export default replyCommentReducer;
