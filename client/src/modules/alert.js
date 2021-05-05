250083ce3555d9ba9b9a52c5cefc9cc67ffbbf3e
Jengyoung <young_pallete@naver.com>
2021-05-05 18:24:11 +0900

    [ CREATE ] conform alert action & reducer

--- client/src/modules/alert.js
+++ client/src/modules/alert.js
@@ -4,6 +4,7 @@ import createSaga from '../lib/createSaga';
 import alertAPI from '../lib/routes/alert/alert';
 import { takeLatest } from 'redux-saga/effects';
 import checkAlertAPI from '../lib/routes/alert/checkAlert';
+import conformAlertAPI from '../lib/routes/alert/conformAlert';
 
 /* create alert action types */ 
 const INITIALIZE_ALERT = `alert/INITIALIZE_ALERT`;
@@ -11,20 +12,25 @@ const ALERT = `alert/ALERT`;
 const [ ALERT_SUCCESS, ALERT_FAILURE ] = createActionTypes(ALERT);
 const CHECK_ALERT = `alert/CHECK_ALERT`;
 const [ CHECK_ALERT_SUCCESS, CHECK_ALERT_FAILURE ] = createActionTypes(CHECK_ALERT);
+const CONFORM_ALERT = `alert/CONFORM_ALERT`
+const [ CONFORM_ALERT_SUCCESS, CONFORM_ALERT_FAILURE ] = createActionTypes(CONFORM_ALERT);
 
 /* create alert aciton creator */ 
 export const initializeAlert = createAction(INITIALIZE_ALERT);
 export const alertUser = createAction(ALERT, params => params);
 export const checkAlertUser = createAction(CHECK_ALERT, user_id => user_id);
+export const conformAlertUser = createAction(CONFORM_ALERT, user_id => user_id);
 
 /* create customized saga */ 
 const alertUserSaga = createSaga(ALERT, alertAPI);
 const checkAlertUserSaga = createSaga(CHECK_ALERT, checkAlertAPI);
+const conformAlertUserSaga = createSaga(CONFORM_ALERT, conformAlertAPI);
 
 /* create alert saga */ 
 export function* alertSaga() {
     yield takeLatest(ALERT, alertUserSaga);
     yield takeLatest(CHECK_ALERT, checkAlertUserSaga);
+    yield takeLatest(CONFORM_ALERT, conformAlertUserSaga);
 };
 
 const initialState = {
@@ -51,6 +57,15 @@ const alertReducer = handleActions({
         alertsError: null,
     }),
     [CHECK_ALERT_FAILURE]: (state, { payload: error}) => ({
+        ...state,
+        alertsError: error,
+    }),
+    [CONFORM_ALERT_SUCCESS]: (state, { payload: alerts }) => ({
+        ...state,
+        alerts,
+        alertsError: null,
+    }),
+    [CONFORM_ALERT_FAILURE]: (state, { payload: error }) => ({
         ...state,
         alertsError: error,
     })
