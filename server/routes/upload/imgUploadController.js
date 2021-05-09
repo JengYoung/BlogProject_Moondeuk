e66cc65d1903f4dbaa46039f19d9ab7c5d52f5c0
Jengyoung <young_pallete@naver.com>
2021-05-09 23:12:07 +0900

    [ CREATE ] imgUploadController

--- server/routes/upload/imgUploadController.js
+++ server/routes/upload/imgUploadController.js
@@ -1,5 +1,28 @@
-const imgUploadController = async (req, res) => {
+import upload from '../../lib/upload.js';
 
+const imgUploadController = async (req, res) => {
+    const { user_id } = req.params;
+    const reqFiles = "";
+    try {
+        upload(req, res, function(arr) {
+            if (err) {
+                return res.status(400).send({
+                    message: err.message,
+                    files: reqFiles
+                });
+            }
+            reqFiles = req.files[0].filename
+            res.status(200).send({
+                message: "Success",
+                files: reqFiles
+            });
+        });
+    } catch(err) {
+        res.status(500).send({
+            message: err,
+            files: reqFiles
+        });
+    }
 }
 
 export default imgUploadController;
