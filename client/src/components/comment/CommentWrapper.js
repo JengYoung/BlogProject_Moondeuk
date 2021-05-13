375cb048b68830d619d0942f41dc22bb68ca2e3f
Jengyoung <young_pallete@naver.com>
2021-05-13 15:51:16 +0900

    [ UPDATE ] CommentWrapper design

--- client/src/components/comment/CommentWrapper.js
+++ client/src/components/comment/CommentWrapper.js
@@ -5,24 +5,27 @@ import styled from 'styled-components';
 */
 
 const StyledCommentWrapper = styled.div`
-    display: none;
+    display: block;
     position: fixed;
     z-index: 50;
     right: 0%;
     bottom: 8vh;
-    background-color: #f8f8f8;
-    width: 400px;
     height: 84vh;
+    background-color: #f8f8f8;
+    width: 100%;
     box-sizing: border-box;
-    transition: all 10s;
+    right: -400px;
+    transition: all 0.5s ease;
     @media screen and (min-width: 481px) {
         height: 80vh;
         bottom: 10vh;
     }
     @media screen and (min-width: 769px) {
         height: 78vh;
+        width: 400px;
     }
     &.open {
+        right: 0;
         background-color: #e3e1e4;
         display: block;
     }
