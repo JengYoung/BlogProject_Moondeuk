90fb1f3e8856f654d54b9f9e743e2fe8a4427000
Jengyoung <young_pallete@naver.com>
2021-05-14 09:59:56 +0900

    [UPDATE] server/ checkCommentController params error fixed

--- server/routes/comment/checkCommentController.js
+++ server/routes/comment/checkCommentController.js
@@ -1,9 +1,11 @@
 import Comment from '../../models/comment.js';
 
 const checkCommentController = async (req, res) => {
-    const { diaryId } = req.params;
+    const { diary_id } = req.params;
+    console.log(diary_id)
     try {
-        const commentList = await Comment.find({ diaryId });
+        const commentList = await Comment.find({ diary_id });
+        console.log("commentList: ",commentList)
         res.send(commentList)
     } catch(e) {
         res.status(400).send(e);
