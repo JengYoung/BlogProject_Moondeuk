cfeba56bf802fba2defcb407538fcd78e7c3f9a8
Jengyoung <young_pallete@naver.com>
2021-05-23 21:18:35 +0900

    [Update] likeController & like 'likeAt' field added

--- server/models/like.js
+++ server/models/like.js
@@ -6,9 +6,15 @@ const likeSchema = new Schema({
     /* 
         like의 타입을 정의. (Diary (default) / Comment / ReplyComment)
     */
-    likeType: {
+    typeName: {
         type: String,
         default: "Diary"
+    },
+    /*
+        Type에서 like를 받고 있는 data의 id
+    */ 
+    typeId: {
+        type: mongoose.Types.ObjectId,
     },
     /* 
         좋아요 한 사람의 아이디 (중복 방지 및 유저 확인)
@@ -20,9 +26,10 @@ const likeSchema = new Schema({
     /*
         타입에 따라서 조회할 대상 고유 아이디
     */
-    likeTypeId: {
+    diaryId: {
         type: mongoose.Types.ObjectId,
-    }
+        ref: 'Diary'
+    },
 });
 
 
--- server/routes/like/likeController.js
+++ server/routes/like/likeController.js
@@ -2,11 +2,16 @@ import Like from '../../models/like.js';
 
 const likeController = async (req, res) => {
     const { userId, diaryId } = req.params;
+    /* typeName: Diary, Comment, ReplyComment */
+    const { typeId, typeName } = req.body;
     const like = new Like({
+        typeName,
+        typeId,
         userId,
         diaryId,
     });
     try {
+        /* 현재 이미 좋아요를 눌렀는 지 확인 */ 
         const checkExist = await Like.findData({ userId, diaryId });
         if (checkExist) return res.status(409).send();
         await like.save();
