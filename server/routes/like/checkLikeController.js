f6a6478594c7d10a3f9d36823f99503f6e4694f4
Jengyoung <young_pallete@naver.com>
2021-05-24 15:40:52 +0900

    [UPDATE] checkLikeController & like module

--- server/routes/like/checkLikeController.js
+++ server/routes/like/checkLikeController.js
@@ -7,8 +7,8 @@ const checkLikeController = async (req, res) => {
         const [diaryTypeResult] = await Like.find({ typeName: 'Diary', userId, diaryId }).exec();
         const [commentTypeResult] = await Like.find({ typeName:"Comment", userId, diaryId }).exec();
         const [replyCommentTypeResult] = await Like.find({ typeName: "ReplyComment", userId, diaryId}).exec()
-        console.log({diaryTypeResult, commentTypeResult, replyCommentTypeResult})
-        // return res.send();
+        // console.log({diaryTypeResult, commentTypeResult, replyCommentTypeResult})
+        return res.send({diaryTypeResult, commentTypeResult, replyCommentTypeResult});
     } catch(e) {
         return res.status(400).send()
     }
